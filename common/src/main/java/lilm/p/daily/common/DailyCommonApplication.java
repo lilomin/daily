6b1060a973740cc1d8c83206ead73ab9877c46ce
lilemin <lilm@uyunsoft.cn>
2018-06-23 20:52:16 +0800

    Structure 2

---
+++ common/src/main/java/lilm/p/daily/common/DailyCommonApplication.java
@@ -0,0 +1,12 @@
+package lilm.p.daily.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyCommonApplication.class, args);
+	}
+}
